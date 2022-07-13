80917a0e85c5bb08be9fbd44040f86897805885d
ZakariyeAden <zakariye5417@gmail>
2022-07-13 16:00:44 -0500

    _

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Home from './Components/Home'
 import Service from './Components/Service'
 import Menu from './Components/Menu'
+import Form from './Components/Form'
 function App() {
   return (
     <div>
       <Home />
       <Service />
       <Menu/>
+      <Form/>
     </div>
   )
 }
