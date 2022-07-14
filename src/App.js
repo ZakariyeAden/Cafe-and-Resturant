87ada900e5c6f64d2ca67026190997b657e7bcd8
ZakariyeAden <zakariye5417@gmail>
2022-07-14 11:55:59 -0500

    implementing props onclick btn for toast

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import Home from './Components/Home'
 import Service from './Components/Service'
 import Menu from './Components/Menu'
 import Form from './Components/Form'
+import Toastify from 'toastify-js'
+import "toastify-js/src/toastify.css"
+import notify from './Components/Toast'
 function App() {
+
   return (
     <div>
       <Home />
       <Service />
       <Menu/>
-      <Form/>
+      <Form onClick={notify}/>
+      
     </div>
   )
 }
