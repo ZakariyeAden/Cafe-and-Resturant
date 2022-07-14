1119134e7cc8369133452851daa4a09f133b29ae
ZakariyeAden <zakariye5417@gmail>
2022-07-14 12:55:56 -0500

    navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,29 +1,25 @@
-import React from 'react'
-
+import React from "react";
+import "../Style/Navbar.css";
+import { Link } from 'react-scroll'
 function Navbar() {
-  const [scrolled, setScrolled] = React.useState(false);
-
-  useLayoutEffect(() => {
-    const handleScroll = () =>
-      window.scrollY > 0 ? setScrolled(true) : setScrolled(false);
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-  
   return (
     <header>
       <nav>
         <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Menu</a></li>
-          <li><a href="#">Reserve</a></li>
+          <li>
+            <Link activeClass="active" to="home" spy={true} smooth={true} offset={50} duration={500}>Home</Link>
+          </li>
+          <li>
+            <Link activeClass="active" to="menu" spy={true} smooth={true} offset={50} duration={500}>Menu</Link>
+          </li>
+          <li>
+            <Link activeClass="active" to="reservation" spy={true} smooth={true} offset={50} duration={500}>Reservation</Link>
+          </li>
+ 
         </ul>
       </nav>
     </header>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
