e3a8af87d8c6744403f9abf67afc749829fd8ea5
ZakariyeAden <zakariye5417@gmail>
2022-07-14 13:46:30 -0500

    adding offset

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -9,16 +9,13 @@ function Navbar() {
           <li>
             <Link activeClass="active" to="home" spy={true} smooth={true} offset={50} duration={500}>Home</Link>
           </li>
+        
           <li>
-            <Link activeClass="active" to="service" spy={true} smooth={true} offset={50} duration={500}>Service</Link>
+            <Link activeClass="active" to="menu" spy={true} smooth={true} offset={700} duration={500}>Menu</Link>
           </li>
           <li>
-            <Link activeClass="active" to="menu" spy={true} smooth={true} offset={50} duration={500}>Menu</Link>
+            <Link activeClass="active" to="reservation" spy={true} smooth={true} offset={2450} duration={500}>Reservation</Link>
           </li>
-          <li>
-            <Link activeClass="active" to="reservation" spy={true} smooth={true} offset={30} duration={500}>Reservation</Link>
-          </li>
- 
         </ul>
       </nav>
     </header>
