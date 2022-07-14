0515f0f113974e2d29f9e9583632eac2a5a30eb1
ZakariyeAden <zakariye5417@gmail>
2022-07-14 18:22:49 -0500

    finished with navbar

--- src/App.js
+++ src/App.js
@@ -6,14 +6,18 @@ import Form from "./Components/Form";
 import "toastify-js/src/toastify.css";
 import notify from "./Components/Toast";
 import Navbar from "./Components/Navbar";
+import { HiOutlineX } from "react-icons/hi";
 function App() {
+
   return (
-    <div>
-      <Navbar />
+    <div id="outerContainer">
+      <Navbar pageWrapId={"page-wrap"} outerContainerId={"outerContainer"} customBurgerIcon={<span className="hamburger"><ion-icon name="menu-outline"></ion-icon></span>} customCrossIcon={<span><HiOutlineX /></span>}/>
+      <main id="page-wrap">
       <Home />
       <Service />
       <Menu />
       <Form onClick={notify} />
+      </main>
     </div>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,23 +1,54 @@
 import React from "react";
 import "../Style/Navbar.css";
-import { Link } from 'react-scroll'
-function Navbar() {
+import { Link } from "react-scroll";
+import { slide as Menu } from "react-burger-menu";
+
+
+function Navbar(props) {
+ 
   return (
     <header>
-      <nav>
+      <Menu {...props}>
         <ul>
           <li>
-            <Link activeClass="active" to="home" spy={true} smooth={true} offset={50} duration={500}>Home</Link>
+            <Link
+              activeClass="active"
+              to="home"
+              spy={true}
+              smooth={true}
+              offset={50}
+              duration={500}
+            >
+              Home
+            </Link>
           </li>
-        
+
           <li>
-            <Link activeClass="active" to="menu" spy={true} smooth={true} offset={700} duration={500}>Menu</Link>
+            <Link
+              activeClass="active"
+              to="menu"
+              spy={true}
+              smooth={true}
+              offset={700}
+              duration={500}
+            >
+              Menu
+            </Link>
           </li>
           <li>
-            <Link activeClass="active" to="reservation" spy={true} smooth={true} offset={2450} duration={500}>Reservation</Link>
+            <Link
+              activeClass="active"
+              to="reservation"
+              spy={true}
+              smooth={true}
+              offset={2450}
+              duration={500}
+            >
+              Reservation
+            </Link>
           </li>
         </ul>
-      </nav>
+      </Menu>
     </header>
   );
 }
